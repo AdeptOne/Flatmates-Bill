4286c3cc91b3c848b993cf0c3c937318f23b8155
Yesntooky <Yesentooky@gmail.com>
2022-01-26 22:52:42 +0500

    updated user experience

--- main.py
+++ main.py
@@ -78,12 +78,22 @@ class PdfReport:
         webbrowser.open(self.filename)
 
 
-the_bill = Bill(amount=120, period="April 2022")
-john = Flatmate(name="John", days_in_house=20)
-marry = Flatmate(name="Marry", days_in_house=25)
+amount = float(input("Enter the bill amount: "))
+period = input("What is the bill period? E.g. December 2020: ")
 
-print('John pays: ', john.pays(bill=the_bill, second_flatmate=marry))
-print('Marry pays: ', marry.pays(bill=the_bill, second_flatmate=john))
+name1 = input("What is your name? ")
+day_in_house1 = int(input(f"How many days did {name1} stay in the house during the bill period? "))
 
-pdf_report = PdfReport(filename="Report.pdf")
-pdf_report.generate(first_flatmate=john, second_flatmate=marry, bill=the_bill)
+name2 = input("What is the name of the other flatmate? ")
+day_in_house2 = int(input(f"How many days did {name2} stay in the house during the bill period? "))
+
+
+the_bill = Bill(amount, period)
+flatmate1 = Flatmate(name1, day_in_house1)
+flatmate2 = Flatmate(name2, day_in_house2)
+
+print(f'{flatmate1.name} pays: ', flatmate1.pays(the_bill, flatmate2))
+print(f'{flatmate2.name} pays: ', flatmate2.pays(the_bill, flatmate1))
+
+pdf_report = PdfReport(filename=f"{the_bill.period}.pdf")
+pdf_report.generate(flatmate1, flatmate2, the_bill)
